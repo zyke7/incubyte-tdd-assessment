23b42573e883d78b18b7080e2a389a6cb36a45ad
Dan <danphilip12@gmail.com>
2024-08-06 11:08:27 +0530

    Added logic and tests for strings with comma and new line

--- string-calculator/stringCalculator.js
+++ string-calculator/stringCalculator.js
@@ -1,9 +1,10 @@
 function add(numbers) {
   if (numbers === "")
     return 0;
+  const delimiter = /[\n,]/;
   const arr = numbers
-    .split(',')
-    .map(num => parseInt(num));
+    .split(delimiter)
+    .map(num => parseInt(num.trim()));
   const sum = arr.reduce((acc, num) => acc + num, 0);
   return sum;
 }
--- string-calculator/stringCalculator.test.js
+++ string-calculator/stringCalculator.test.js
@@ -11,3 +11,11 @@ test('returns the number itself if only one number is provided', () => {
 test('returns the sum of two numbers', () => {
   expect(add('1,2')).toBe(3);
 })
+
+test('returns the sum of multiple numbers', () => {
+  expect(add('1, 2, 3, 4, 5')).toBe(15);
+})
+
+test('returns the sum of numbers with new lines', () => {
+  expect(add('1\n2,3')).toBe(6);
+})
