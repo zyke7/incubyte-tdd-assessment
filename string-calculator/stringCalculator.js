1e83b8fd7edf9cf9c9e06212589ed751ecf956b2
Dan <danphilip12@gmail.com>
2024-08-06 10:42:13 +0530

    Added logic and tests for single and comma separated numbers

--- string-calculator/stringCalculator.js
+++ string-calculator/stringCalculator.js
@@ -1,7 +1,11 @@
 function add(numbers) {
   if (numbers === "")
     return 0;
-
+  const arr = numbers
+    .split(',')
+    .map(num => parseInt(num));
+  const sum = arr.reduce((acc, num) => acc + num, 0);
+  return sum;
 }
 
 module.exports = add;
--- string-calculator/stringCalculator.test.js
+++ string-calculator/stringCalculator.test.js
@@ -1,6 +1,13 @@
 const add = require('./stringCalculator');
 
 test('returns 0 for an empty string', () => {
-    expect(add("")).toBe(0);
+    expect(add('')).toBe(0);
 });
 
+test('returns the number itself if only one number is provided', () => {
+  expect(add('1')).toBe(1); 
+})
+
+test('returns the sum of two numbers', () => {
+  expect(add('1,2')).toBe(3);
+})
